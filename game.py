9bd533c77c614a4912baec9afa013b785c366d46
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-05-28 09:10:26 +0200

    checking for winner done

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import random
 print("WELCOME TO ROCK,PAPER,SCISSORS GAME")
 
 def info():
@@ -10,10 +11,46 @@ def info():
 def game():
     hand = input("Please enter either r for rock, p for paper or s for scissors: ")
     options = ["r", "p", "s"]
-    
-    if hand in options:
-        print("yaay")
+    while hand != "s" and hand != "r" and hand != "p":
+        hand = input("Please enter either r for rock, p for paper or s for scissors: ")
     else:
-        print("nah")
+        machine_move = random.randint(0,2)
+        machine_move = options[machine_move]
+        if (hand == "r" ):
+            if (machine_move == "p"):
+                print("Machine wins!!!")
+                print(machine_move)
+            elif (machine_move == "s"):
+                print("You win")
+                print(machine_move)
+            elif(machine_move == "r"):
+                print("It's a tie")
+                print(machine_move)
+
+        elif (hand == "p" ):
+            if (machine_move == "s"):
+                print("Machine wins!!!")
+                print(machine_move)
+            elif (machine_move == "r"):
+                print("You win")
+                print(machine_move)
+            elif(machine_move == "p"):
+                print("It's a tie")
+                print(machine_move)
+
+        elif (hand == "s" ):
+            if (machine_move == "r"):
+                print("Machine wins!!!")
+                print(machine_move)
+            elif (machine_move == "p"):
+                print("You win")
+                print(machine_move)
+            elif(machine_move == "s"):
+                print("It's a tie")
+                print(machine_move)
+        
+        
+
+   
 
 game()
